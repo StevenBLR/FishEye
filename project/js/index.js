285d1c471f8c7610a763497a587819507dc13b02
@StevenBLR <s.belair95@gmail.com>
2021-04-23 13:20:29 +0200

    Fine tuning responsivness index

--- project/js/index.js
+++ project/js/index.js
@@ -632,7 +632,6 @@ var allTags = new Map();
 
 // DOM Elements
 var keywordsRoot = document.querySelector(".header__keywords");
-var keywordsWrapper = document.querySelector(".header__keywords-wrapper");
 var photographersRoot = document.querySelector(".photographers__list");
 
 // Store every data from Json into 2 objects --> photographers[] / medias[]
