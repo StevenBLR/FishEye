0a24109c415959152d7de70427396c9687298a52
StevenBLR <35036528+StevenBLR@users.noreply.github.com>
2021-04-25 15:26:40 +0200

    05 - Ajouter les styles interactifs ( hover / clic etc .. )

--- project/js/index.js
+++ project/js/index.js
@@ -713,21 +713,12 @@ function InitTagsEvents(){
 // Update page without reloading
 function RefreshPage(clickedElt){
   currentValues = clickedElt.getAttribute('data-id');
-  console.log(currentValues);
+  //console.log(currentValues);
   // Reset all tags style ans update clicked tag style
-  console.log("All header tags spans = ", keywordsRoot.getElementsByTagName('span'));
-  // keywordsRoot.getElementsByTagName('span').forEach(elt => { 
-  //   elt.removeAttribute("id");
-  // });
+  // To iterate a child object, we need to use Array.prototype
   Array.prototype.forEach.call(keywordsRoot.getElementsByTagName('span'), elt => {
     elt.removeAttribute("id");
   });
-
-  // var allHeaderTags = [];
-  // allHeaderTags.push(keywordsRoot.getElementsByTagName('span'));
-  // allHeaderTags.forEach(elt => { 
-  //   elt.removeAttribute('id');
-  // });
   clickedElt.getElementsByTagName('span')[0].setAttribute("id", "selected");
   PopulatePhotographers();
 }
@@ -740,7 +731,7 @@ function PopulatePhotographers(){
     photographersToShow = [];
     photographers.forEach(p => {
       if(p.tags.includes(currentValues.toString())){
-        console.log("Adding " + p.name);
+        //console.log("Adding " + p.name);
         photographersToShow.push(p);
       }
     });
