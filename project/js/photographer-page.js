120c6b7a9e7ca507120edae72ab83daf2422532b
@StevenBLR <s.belair95@gmail.com>
2021-04-29 17:37:41 +0200

    Js hover system

--- project/js/photographer-page.js
+++ project/js/photographer-page.js
@@ -1,3 +1,13 @@
 
-const filterDropdown = document.querySelector(".dropdown");
+const filterDropdown = document.querySelectorAll(".dropdown");
 console.log(filterDropdown);
+
+filterDropdown.forEach(elt => {
+    elt.addEventListener("mouseenter", function(e){
+        //console.log("enter " + elt.id);
+    });
+
+    elt.addEventListener("mouseleave", function(e){
+        //console.log("leave " + elt.id);
+    })
+})
