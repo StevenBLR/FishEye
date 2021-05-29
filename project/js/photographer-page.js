f681b344bccf75c9e31c0f0259731cffbc017a61
@StevenBLR <s.belair95@gmail.com>
2021-05-29 16:15:47 +0200

    Setting up dropdown behaviour

--- project/js/photographer-page.js
+++ project/js/photographer-page.js
@@ -2,13 +2,21 @@
 const filterDropdown = document.querySelectorAll(".dropdown");
 console.log(filterDropdown);
 
+// const ddAnim = gsap.timeline({reversed: true, paused:true})
+// .set(".open", {autoAlpha:0})
+// .to(".test", {height: "auto", duration: 1.5})
+// .from(".list", {autoAlpha:0, x:50, duration:1}, '-=1')
+
 filterDropdown.forEach(elt => {
     elt.addEventListener("mouseenter", function(e){
         //console.log("enter " + elt.id);
+        //let 
+        gsap.to(".dropdown", {height: 123, duration: 1, ease: "expo"});
     });
 
     elt.addEventListener("mouseleave", function(e){
         //console.log("leave " + elt.id);
+        gsap.to(".dropdown", {height: 40, duration: 1, ease: "expo"});
     })
 })
 
